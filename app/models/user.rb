c878253d036eac2f7da63a53196635a1d431de54
Konrad Makowski <konrad@snopkow.eu>
2019-03-11 13:42:07 +0100

    ustawia role nowego użyszkodnika

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   after_create :set_instytucja
+  after_create :set_role
+  
   belongs_to :instytucja
 
   def role?(type)
@@ -14,6 +16,10 @@ class User < ApplicationRecord
 
   def set_instytucja
     self.update(instytucja_id: "58bd02b5-ffbd-4226-b512-71af8078df9f")
+  end
+
+  def set_role
+    self.update(role: :specjalista)
   end
   
 end
