888fb947b25de0601267b6701e7570774958696a
Konrad Makowski <konrad@snopkow.eu>
2019-03-11 13:27:47 +0100

    nowy user trafia do testowej firmy;

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :set_instytucja
   belongs_to :instytucja
 
   def role?(type)
     self.role.underscore.to_sym == type
+  end
+
+  def set_instytucja
+    self.update(instytucja_id: "58bd02b5-ffbd-4226-b512-71af8078df9f")
   end
   
 end
