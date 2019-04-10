634656f6e9d240af90664248d363306df85c23c7
Konrad Makowski <konrad@snopkow.eu>
2019-04-10 09:48:56 +0200

    Jeśli słoma zebrana to nie dodawaj 21 do N

--- app/models/uzytek.rb
+++ app/models/uzytek.rb
@@ -115,7 +115,7 @@ class Uzytek < ApplicationRecord
 
   # zapotrzebowanie rośliny w kg N/1 ha
   def zapotrzebowanie_ha
-    plon * roslina.pobranie + if roslinaprzedplon.id > 2 then roslinaprzedplon.n else 0 end
+    plon * roslina.pobranie + if (roslinaprzedplon.id > 2 && stanprzedplonu == false) then roslinaprzedplon.n else 0 end
   end
 
   # PRAWA STRONA
